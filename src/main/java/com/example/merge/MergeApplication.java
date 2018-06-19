96c775910f9dca34ae8ae747bb60ef1535bf91af
nishithay88 <rohith.j721@gmail.com>
2018-06-19 12:45:57 -0500

    first commit

---
+++ src/main/java/com/example/merge/MergeApplication.java
@@ -0,0 +1,12 @@
+package com.example.merge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MergeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MergeApplication.class, args);
+    }
+}
