cda9ac6de20cf721a324667c2b3faf837058e611
nishithay88 <rohith.j721@gmail.com>
2018-06-19 14:49:42 -0500

    my second commit

---
+++ src/main/java/com/example/merge/Rohith.java
@@ -0,0 +1,13 @@
+package com.example.merge;
+
+public class Rohith {
+    private String nam;
+
+    public String getNam() {
+        return nam;
+    }
+
+    public void setNam(String nam) {
+        this.nam = nam;
+    }
+}
