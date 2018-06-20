e8cc5ea680032dae721db85854f760387c3937bf
nishithay88 <rohith.j721@gmail.com>
2018-06-20 09:55:40 -0500

    new class

---
+++ src/main/java/com/example/merge/Rohith1.java
@@ -0,0 +1,14 @@
+package com.example.merge;
+
+public class Rohith1 {
+    private String n;
+
+
+    public String getN() {
+        return n;
+    }
+
+    public void setN(String n) {
+        this.n = n;
+    }
+}
